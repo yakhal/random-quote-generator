3af37fffce060b3a7b35d956487643d2d0888879
yakhal <rastogi.p39@gmail.com>
2022-02-17 15:52:23 +0530

    Added header and credits as an enhancement, fixes #6

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Card from "./components/Card";
 
 function App() {
   const [data, setData] = useState({
-    quote:"Time is limited, so don't waste it living someone else's life.",
+    quote: "Time is limited, so don't waste it living someone else's life.",
     author: "Steve Jobs"
   })
 
@@ -28,8 +28,12 @@ function App() {
   // const classes = classNames("min-h-screen flex items-center", "bg-"+data.color)
 
   return (
-    <div className="min-h-screen flex items-center bg-indigo-500 px-5">
-      <Card data={data} getQuote={getNewQuoteHandler}></Card>
+    <div className="min-h-screen bg-indigo-500 px-5 flex items-center">
+      <div className="max-w-lg mx-auto">
+        <h1 className="text-3xl text-center font-bold text-white mb-5 font-sans italic">PickQuotes</h1>
+        <Card data={data} getQuote={getNewQuoteHandler}></Card>
+        <p className="italic text-sm text-white/80 text-center mt-4 ">By <a className="underline hover:no-underline" href="https://twitter.com/prakharrrastogi">Prakhar</a></p>
+      </div>
     </div>
   );
 }
