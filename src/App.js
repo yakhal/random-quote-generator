ea35083221f2461690ed3c781c575a591c92d9f6
yakhal <rastogi.p39@gmail.com>
2022-02-21 16:45:53 +0530

    Generates new quote on every page load, fixes #3

--- src/App.js
+++ src/App.js
@@ -1,42 +1,72 @@
-import { useState } from "react";
+import React from "react";
 import Card from "./components/Card";
 
-function App() {
-  const getRandomColor = () => {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "",
+      author: "",
+      color: "white"
+    }
+    this.getNewQuoteHandler = this.getNewQuoteHandler.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="min-h-screen px-5 flex items-center" style={{ backgroundColor: this.state.color, color: this.state.color }}>
+        <div className="max-w-lg mx-auto">
+          <h1 className="text-3xl text-center font-bold text-white mb-5 font-sans italic">PickQuotes</h1>
+          <Card data={this.state} getQuote={this.getNewQuoteHandler}></Card>
+          <p className="italic text-sm text-white/80 text-center mt-4 ">By <a className="underline hover:no-underline" href="https://twitter.com/prakharrrastogi">Prakhar</a></p>
+        </div>
+      </div>
+    );
+  }
+
+  // Retrieve data from API and setState when the component mounted
+  componentDidMount() {
+    if (!this.state.quote) {
+      (async () => {
+        try {
+          this.setState(await this.getQuoteData());
+        } catch (e) {
+          console.log(e)
+        }
+      })();
+    }
+  }
+
+  // Generates Random color 
+  getRandomColor() {
     const colors = ["#737373", "#ef4444", "#f97316", "#ca8a04", "#84cc16", "#10b981", "#0ea5e9", "#6366f1", "#d946ef", "#fb7185"]
     return colors[Math.floor(Math.random() * colors.length)];
   }
 
-  const [data, setData] = useState({
-    quote: "Time is limited, so don't waste it living someone else's life.",
-    author: "Steve Jobs",
-    color: getRandomColor()
-  })
+  // Used to handle "New Quote" button click 
+  async getNewQuoteHandler() {
+    const data = await this.getQuoteData();
+    this.setState(data);
+  }
 
-  async function getNewQuoteHandler() {
+  // Used to retrieve data from API
+  async getQuoteData() {
+    let quote = {
+      quote: "Some Error occured, please try again later.",
+      author: "Error",
+      color: this.getRandomColor()
+    };
     const response = await fetch("https://api.quotable.io/random");
     const data = await response.json();
     if (response.ok) {
-      const quote = {
+      quote = { 
         quote: data.content,
         author: data.author,
-        color: getRandomColor()
+        color: this.getRandomColor()
       };
-      setData(quote);
-    } else {
-      console.log("Unexpected Error Occured")
-    }
+    } 
+    return quote;
   }
-
-  return (
-    <div className="min-h-screen px-5 flex items-center" style={{backgroundColor: data.color, color:data.color}}>
-      <div className="max-w-lg mx-auto">
-        <h1 className="text-3xl text-center font-bold text-white mb-5 font-sans italic">PickQuotes</h1>
-        <Card data={data} getQuote={getNewQuoteHandler}></Card>
-        <p className="italic text-sm text-white/80 text-center mt-4 ">By <a className="underline hover:no-underline" href="https://twitter.com/prakharrrastogi">Prakhar</a></p>
-      </div>
-    </div>
-  );
 }
 
 export default App;
